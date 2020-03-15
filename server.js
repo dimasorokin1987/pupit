5159757df1064517a992305b5f9920ff1d0a4e31
Dima <Dima@Ilyas-MacBook-Air.local>
2020-03-15 21:20:02 +0200

    add custom basic auth

--- server.js
+++ server.js
@@ -3,39 +3,75 @@ const express = require('express');
 
 let browser = undefined;
 let page = undefined;
+let token = undefined;
 let isOpened = false;
+let isNavigated = false;
+const pass = 'pass';
 
 const wait = tm=>new Promise(res=>setTimeout(res,tm));
+const hashCode = s => s.split('').reduce((a,b) => (((a << 5) - a) + b.charCodeAt(0))|0, 0)
+
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 
+app.get('/getToken', async (req, res) => {try{
+    if(isOpened){
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('token:'+token);
+    }else{
+        res.end();
+    }
+}catch(e){res.end(e.toString())}});
+
+app.get('/checkToken', async (req, res) => {try{
+    const hasAuth = String(req.query.token) === String(token);
+    if(isOpened){
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('status:'+hasAuth);
+    }else{
+        res.end();
+    }
+}catch(e){res.end(e.toString())}});
+
 //http://127.0.0.1:4000/open/?w=640&h=480
 app.get('/open', async (req, res) => {try{
-    browser = await puppeteer.launch({
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        executablePath: '/usr/bin/google-chrome'
-    });
-    page = await browser.newPage();
-    const width = Number(req.query.w) || 640;
-    const height = Number(req.query.h) || 480;
-    await page.setViewport({width,height});
-    isOpened = true;
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('opened');
+    if(!isOpened){
+        browser = await puppeteer.launch({
+          //  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+          //  executablePath: '/usr/bin/google-chrome'
+        });
+        page = await browser.newPage();
+        const width = Number(req.query.w) || 640;
+        const height = Number(req.query.h) || 480;
+        await page.setViewport({width,height});
+        isOpened = true;
+        let randStr = Math.random().toString().slice(2);
+        token = hashCode(pass+randStr);
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('opened,rnd='+randStr);
+    }else{
+        res.end();
+    }
 }catch(e){res.end(e.toString())}});
 
 //http://127.0.0.1:4000/goto/?url=http://127.0.0.1:5000
 app.get('/goto', async (req, res) => {try{
-    await page.goto(req.query.url);
-    isOpened = true;
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('navigated');
+    const hasAuth = String(req.query.token) === String(token);
+    if(hasAuth && isOpened){
+        await page.goto(req.query.url);
+        isNavigated = true;
+        res.writeHead(200,{'Content-Type':'text/plain'});
+        res.end('navigated');
+    }else{
+        res.end();
+    }
 }catch(e){res.end(e.toString())}});
 
 app.get('/screenshot', async (req, res) => {try{
-    if(isOpened){
+    const hasAuth = String(req.query.token) === String(token);
+    if(hasAuth && isNavigated){
         const screenshotBuffer = await page.screenshot();
         res.writeHead(200, {
             'Content-Type': 'image/png',
@@ -48,7 +84,8 @@ app.get('/screenshot', async (req, res) => {try{
 }catch(e){res.end(e.toString())}});
 
 app.get('/click', async (req, res) => {try{
-    if(isOpened){
+    const hasAuth = String(req.query.token) === String(token);
+    if(hasAuth && isNavigated){
         let x=Number(req.query.x);
         let y=Number(req.query.y);
         await page.mouse.click(x, y, {delay: 500});
@@ -60,7 +97,8 @@ app.get('/click', async (req, res) => {try{
 }catch(e){res.end(e.toString())}});
 
 app.get('/enterText', async (req, res) => {try{
-    if(isOpened){
+    const hasAuth = String(req.query.token) === String(token);
+    if(hasAuth && isNavigated){
         await page.keyboard.type(req.query.txt, {delay: 100});
         res.writeHead(200,{'Content-Type':'text/plain'});
         res.end('text entered');
@@ -70,7 +108,8 @@ app.get('/enterText', async (req, res) => {try{
 }catch(e){res.end(e.toString())}});
 
 app.get('/pressKey', async (req, res) => {try{
-    if(isOpened){
+    const hasAuth = String(req.query.token) === String(token);
+    if(hasAuth && isNavigated){
         await page.keyboard.press(req.query.key, {delay: 200});
         res.writeHead(200,{'Content-Type':'text/plain'});
         res.end('key pressed');
@@ -80,7 +119,8 @@ app.get('/pressKey', async (req, res) => {try{
 }catch(e){res.end(e.toString())}});
 
 app.get('/setBackgroundColor', async (req, res) => {try{
-    if(isOpened){
+    const hasAuth = String(req.query.token) === String(token);
+    if(hasAuth && isNavigated){
         //res.end(req.query.color);
         const color = req.query.color||'#000';
         
@@ -97,9 +137,11 @@ app.get('/setBackgroundColor', async (req, res) => {try{
 
 
 app.get('/close', async (req, res) => {try{
-    if(isOpened){
+    const hasAuth = String(req.query.token) === String(token);
+    if(hasAuth && isOpened){
         await browser.close();
         isOpened = false;
+        isNavigated = false;
         res.writeHead(200,{'Content-Type': 'text/plain'});
         res.end('closed');
     }else{
